24a5dae09379ed8a8617939dd00945522d18389b
Mohit Deshpande <mohitdeshpande@icloud.com>
2017-02-13 19:52:15 -0500

    Initial Commit

---
+++ src/com/humber/java/Main.java
@@ -0,0 +1,12 @@
+package com.humber.java;
+
+/**
+ * Created by mohit on 2017-02-13.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+
+}
